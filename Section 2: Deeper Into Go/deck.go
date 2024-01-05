c800acc548a451b52498a8fd658481d06dca9fe2
pandeyshubham03 <shubham.p20062003@gmail.com>
2024-01-05 22:45:25 +0530

    Adding new method for random integer

--- Section 2: Deeper Into Go/deck.go
+++ Section 2: Deeper Into Go/deck.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of 'deck'
@@ -60,6 +61,10 @@ func newDeckFromFile(filename string) deck {
 }
 
 func (d deck) shuffle() {
+
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
 	for i := range d {
 		newPosition := rand.Intn(len(d) - 1)
 		d[i], d[newPosition] = d[newPosition], d[i] //Swap
