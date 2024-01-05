4054ed84bf2de610e0893da99c2fc7189234772e
pandeyshubham03 <shubham.p20062003@gmail.com>
2024-01-05 13:54:49 +0530

    Changing index variable with _

--- Section 2: Deeper Into Go/deck.go
+++ Section 2: Deeper Into Go/deck.go
@@ -13,11 +13,12 @@ func newDeck() deck {
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
-	for i, suit := range cardSuits {
-		for j, value := range cardValues {
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
 			cards = append(cards, value+" of "+suit)
 		}
 	}
+	return cards
 }
 
 func (d deck) print() {
