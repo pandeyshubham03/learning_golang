3d3d76c43fc0e1c11eff8d619cadf5e21a27356c
pandeyshubham03 <shubham.p20062003@gmail.com>
2024-01-05 15:30:36 +0530

    Adding new deck from file function

--- Section 2: Deeper Into Go/cards.go
+++ Section 2: Deeper Into Go/cards.go
@@ -2,9 +2,7 @@ package main
 
 func main() {
 	// var card string = "Ace of Spades"
-	cards := newDeck()
-	// card = "Five of Diamonds"
-	cards.saveToFile("my_cards")
+	cards := newDeckFromFile
 
 }
 
