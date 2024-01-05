546ddbea3be5f09f7619550d872599b0e32f8d94
pandeyshubham03 <shubham.p20062003@gmail.com>
2024-01-05 23:38:51 +0530

    file changed

--- Section 2: Deeper Into Go/deck_test.go
+++ Section 2: Deeper Into Go/deck_test.go
@@ -1,19 +1,20 @@
 package main
 
 import "testing"
-func TestNewDeck(t *testing.T)   {
-	d: = newDeck()
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
 
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 20, but go %v", len(d))
 	}
 
-	if d[0] != "Ace of Spades"   {
+	if d[0] != "Ace of Spades" {
 		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
 	}
 
-	if d[len(d)- 1] != "Four of Clubs"  {
-		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d) - 1])
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d)-1])
 	}
 
-	}
+}
