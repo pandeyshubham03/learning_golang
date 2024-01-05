0129df404ee6e2d35ded60f188568bbbb8cc5551
pandeyshubham03 <shubham.p20062003@gmail.com>
2024-01-05 15:13:29 +0530

    Adding save to file function

--- Section 2: Deeper Into Go/cards.go
+++ Section 2: Deeper Into Go/cards.go
@@ -4,10 +4,7 @@ func main() {
 	// var card string = "Ace of Spades"
 	cards := newDeck()
 	// card = "Five of Diamonds"
-	hand, remainingCards := deal(cards, 5)
-
-	hand.print()
-	remainingCards.print()
+	cards.saveToFile("my_cards")
 
 }
 
