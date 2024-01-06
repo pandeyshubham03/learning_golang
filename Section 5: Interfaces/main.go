fa9b95f9edb44ba59d8b41b300423f620e74b123
pandeyshubham03 <shubham.p20062003@gmail.com>
2024-01-06 19:11:42 +0530

    Editing Interface file

--- Section 5: Interfaces/main.go
+++ Section 5: Interfaces/main.go
@@ -1,18 +1,39 @@
 package main
 
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
 type englishBot struct{}
 type spanishBot struct{}
 
 func main() {
+	eb := englishBot{}
+	sp := spanishBot{}
 
+	printGreeting(eb)
+	printGreeting(sp)
 }
 
-func (eb englishBot) getGreeting() string {
+// func printGreeting(eb englishBot) {
+// 	fmt.Println(eb.getGreeting())
+// }
+
+// func printGreeting(sp spanishBot) {
+// 	fmt.Println(sp.getGreeting())
+// }
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
 	// very custom logic for generating an english greeting
 	return "Hi There!"
 }
 
-func (sp spanishBot) getGreeting() string {
+func (spanishBot) getGreeting() string {
 	// very custom logic for generating an Spanish greeting
 	return "Hola!"
 }
