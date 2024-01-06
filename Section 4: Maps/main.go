77cfe939d4b12b8d7b5544b8af90394d9cbe36ca
pandeyshubham03 <shubham.p20062003@gmail.com>
2024-01-06 13:09:18 +0530

    Diiferent methods of defining Maps

--- Section 4: Maps/main.go
+++ Section 4: Maps/main.go
@@ -3,11 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#00ff00",
-		"blue":  "#0000ff",
-	}
+
+	// method 1
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#00ff00",
+	// 	"blue":  "#0000ff",
+	// }
+
+	// method 2
+	// var colors map[string]string
+
+	// method 3
+	colors := make(map[string]string)
+
 	fmt.Println(colors)
 
 }
