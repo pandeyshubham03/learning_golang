56360106dfd6dfc47bbb49851f412d7837f522e1
pandeyshubham03 <shubham.p20062003@gmail.com>
2024-01-06 13:16:37 +0530

    Adding function to print map

--- Section 4: Maps/main.go
+++ Section 4: Maps/main.go
@@ -5,18 +5,26 @@ import "fmt"
 func main() {
 
 	// method 1
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#00ff00",
-	// 	"blue":  "#0000ff",
-	// }
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"blue":  "#0000ff",
+	}
 
 	// method 2
 	// var colors map[string]string
 
 	// method 3
-	colors := make(map[string]string)
+	// colors := make(map[string]string)
+	// colors["white"] = "#ffffff"
+	// delete(colors, "white")
 
-	fmt.Println(colors)
+	printMap(colors)
 
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex Code for", color, "is", hex)
+	}
 }
