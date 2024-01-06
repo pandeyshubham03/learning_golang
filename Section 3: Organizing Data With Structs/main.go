e26d7099d59c5ce27ffb2d653cb523eebd70812b
pandeyshubham03 <shubham.p20062003@gmail.com>
2024-01-06 11:28:59 +0530

    Embedding Structs

--- Section 3: Organizing Data With Structs/main.go
+++ Section 3: Organizing Data With Structs/main.go
@@ -2,20 +2,38 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	// defining a struct
+	email   string
+	zipCode int
+}
+
 type person struct {
 	// defining a struct
 	firstName string
 	lastName  string
+	contact contactInfo
 }
 
 func main() {
 	// alex := person{firstName: "Alex", lastName: "Anderson"}
 	// fmt.Println(alex)
-	var alex person
+	// var alex person
 
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
 
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	// fmt.Println(alex)
+	// fmt.Printf("%+v", alex)
+
+	jim := person {
+		firstName: "Jim",
+		lastName: "Party",
+		contact: contactInfo {
+			email: "jim@gmail.com",
+			zipCode: 9400,
+		}
+	}
+
+
 }
